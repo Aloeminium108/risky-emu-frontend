428eec811a4c3464151dc85b15e5a6fe59e4be11
Alice Parrish <alice@pop-os.localdomain>
2023-07-20 15:06:59 -0400

    Created CurrentUser context

---
+++ src/context/CurrentUser.tsx
@@ -0,0 +1,38 @@
+import { FunctionComponent, ReactNode, createContext, useEffect, useState } from "react";
+
+export interface User {
+
+}
+
+export const CurrentUser = createContext<User>({});
+
+type Props = {
+  children: ReactNode
+}
+
+const CurrentUserProvider: FunctionComponent<Props> = (props: Props) => {
+
+  const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
+
+    const getLoggedInUser = async () => {
+      let response = await fetch(`${process.env.REACT_APP_SERVER_URL}/authentication/profile`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      let user = await response.json()
+      setCurrentUser(user)
+    }
+
+    getLoggedInUser()
+  }, [])
+
+  return (
+    <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+      {props.children}
+    </CurrentUser.Provider>
+  )
+}
+
+export default CurrentUserProvider
