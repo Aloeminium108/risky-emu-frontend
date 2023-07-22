997519769c1d5e5396ab720aba57a4bea1680e8b
Alice Parrish <alice@pop-os.localdomain>
2023-07-22 09:45:51 -0400

    Grouped registers in more intuitive format and added reset button

--- src/components/Emulator/Emulator.tsx
+++ src/components/Emulator/Emulator.tsx
@@ -1,15 +1,25 @@
 import { CPU } from "@aloeminium108/risc-v-emulator/dist/cpu"
 import React, { FunctionComponent, useRef, useState } from "react"
+import '../../styles/Emulator.scss'
 
 type Props = { 
   binary: ArrayBuffer
 }
 
+function extendBinary(binary: ArrayBuffer, size: number){
+  const extendedBin = new ArrayBuffer(size);
+  new Uint8Array(extendedBin).set(new Uint8Array(binary));
+  return extendedBin;
+}
+
 const Emulator: FunctionComponent<Props> = (props) => {
 
-  const emulatorRef = useRef<CPU>(new CPU(props.binary, 0));
+  const emulatorRef = useRef<CPU>(new CPU(extendBinary(props.binary, 1024), 0));
+
+  const [ complete, setComplete ] = useState<boolean>(false);
 
   const [ registerSet, setRegisterSet ] = useState<{
+    pc: number,
     ra: number,
     sp: number,
     gp: number,
@@ -42,6 +52,7 @@ const Emulator: FunctionComponent<Props> = (props) => {
     t5: number,
     t6: number,
   }>({
+    pc: emulatorRef.current.pc,
     ra: emulatorRef.current.registerSet.getRegister(1),
     sp: emulatorRef.current.registerSet.getRegister(2),
     gp: emulatorRef.current.registerSet.getRegister(3),
@@ -76,9 +87,52 @@ const Emulator: FunctionComponent<Props> = (props) => {
   });
 
   const executeProgram = () => {
-    console.log(emulatorRef.current.ram);
     emulatorRef.current.executionStep();
     setRegisterSet({
+      pc: emulatorRef.current.pc,
+      ra: emulatorRef.current.registerSet.getRegister(1),
+      sp: emulatorRef.current.registerSet.getRegister(2),
+      gp: emulatorRef.current.registerSet.getRegister(3),
+      tp: emulatorRef.current.registerSet.getRegister(4),
+      t0: emulatorRef.current.registerSet.getRegister(5),
+      t1: emulatorRef.current.registerSet.getRegister(6),
+      t2: emulatorRef.current.registerSet.getRegister(7),
+      s0: emulatorRef.current.registerSet.getRegister(8),
+      s1: emulatorRef.current.registerSet.getRegister(9),
+      a0: emulatorRef.current.registerSet.getRegister(10),
+      a1: emulatorRef.current.registerSet.getRegister(11),
+      a2: emulatorRef.current.registerSet.getRegister(12),
+      a3: emulatorRef.current.registerSet.getRegister(13),
+      a4: emulatorRef.current.registerSet.getRegister(14),
+      a5: emulatorRef.current.registerSet.getRegister(15),
+      a6: emulatorRef.current.registerSet.getRegister(16),
+      a7: emulatorRef.current.registerSet.getRegister(17),
+      s2: emulatorRef.current.registerSet.getRegister(18),
+      s3: emulatorRef.current.registerSet.getRegister(19),
+      s4: emulatorRef.current.registerSet.getRegister(20),
+      s5: emulatorRef.current.registerSet.getRegister(21),
+      s6: emulatorRef.current.registerSet.getRegister(22),
+      s7: emulatorRef.current.registerSet.getRegister(23),
+      s8: emulatorRef.current.registerSet.getRegister(24),
+      s9: emulatorRef.current.registerSet.getRegister(25),
+      s10: emulatorRef.current.registerSet.getRegister(26),
+      s11: emulatorRef.current.registerSet.getRegister(27),
+      t3: emulatorRef.current.registerSet.getRegister(28),
+      t4: emulatorRef.current.registerSet.getRegister(29),
+      t5: emulatorRef.current.registerSet.getRegister(30),
+      t6: emulatorRef.current.registerSet.getRegister(31)
+    });
+    if (emulatorRef.current.pc >= props.binary.byteLength) {
+      setComplete(true);
+      return;
+    }
+  }
+
+  const resetEmulator = () => {
+    emulatorRef.current = new CPU(extendBinary(props.binary, 1024), 0);
+    setComplete(false);
+    setRegisterSet({
+      pc: emulatorRef.current.pc,
       ra: emulatorRef.current.registerSet.getRegister(1),
       sp: emulatorRef.current.registerSet.getRegister(2),
       gp: emulatorRef.current.registerSet.getRegister(3),
@@ -115,41 +169,56 @@ const Emulator: FunctionComponent<Props> = (props) => {
 
   return (
     <div id="emulator">
-      <div>
+      <div id="register-display">
         <h2>Registers</h2>
-        <p>ra: {registerSet.ra}</p>
-        <p>sp: {registerSet.sp}</p>
-        <p>gp: {registerSet.gp}</p>
-        <p>tp: {registerSet.tp}</p>
-        <p>t0: {registerSet.t0}</p>
-        <p>t1: {registerSet.t1}</p>
-        <p>t2: {registerSet.t2}</p>
-        <p>s0: {registerSet.s0}</p>
-        <p>s1: {registerSet.s1}</p>
-        <p>a0: {registerSet.a0}</p>
-        <p>a1: {registerSet.a1}</p>
-        <p>a2: {registerSet.a2}</p>
-        <p>a3: {registerSet.a3}</p>
-        <p>a4: {registerSet.a4}</p>
-        <p>a5: {registerSet.a5}</p>
-        <p>a6: {registerSet.a6}</p>
-        <p>a7: {registerSet.a7}</p>
-        <p>s2: {registerSet.s2}</p>
-        <p>s3: {registerSet.s3}</p>
-        <p>s4: {registerSet.s4}</p>
-        <p>s5: {registerSet.s5}</p>
-        <p>s6: {registerSet.s6}</p>
-        <p>s7: {registerSet.s7}</p>
-        <p>s8: {registerSet.s8}</p>
-        <p>s9: {registerSet.s9}</p>
-        <p>s10: {registerSet.s10}</p>
-        <p>s11: {registerSet.s11}</p>
-        <p>t3: {registerSet.t3}</p>
-        <p>t4: {registerSet.t4}</p>
-        <p>t5: {registerSet.t5}</p>
-        <p>t6: {registerSet.t6}</p>
+        <div>
+          <p>pc: {registerSet.pc}</p>
+        </div>
+        <div>
+          <p>ra: {registerSet.ra}</p>
+          <p>sp: {registerSet.sp}</p>
+          <p>gp: {registerSet.gp}</p>
+          <p>tp: {registerSet.tp}</p>
+        </div>
+        <div>
+          <p>t0: {registerSet.t0}</p>
+          <p>t1: {registerSet.t1}</p>
+          <p>t2: {registerSet.t2}</p>
+          <p>t3: {registerSet.t3}</p>
+          <p>t4: {registerSet.t4}</p>
+          <p>t5: {registerSet.t5}</p>
+          <p>t6: {registerSet.t6}</p>
+        </div>
+        <div>
+          <p>s0: {registerSet.s0}</p>
+          <p>s1: {registerSet.s1}</p>
+          <p>s2: {registerSet.s2}</p>
+          <p>s3: {registerSet.s3}</p>
+          <p>s4: {registerSet.s4}</p>
+          <p>s5: {registerSet.s5}</p>
+          <p>s6: {registerSet.s6}</p>
+          <p>s7: {registerSet.s7}</p>
+          <p>s8: {registerSet.s8}</p>
+          <p>s9: {registerSet.s9}</p>
+          <p>s10: {registerSet.s10}</p>
+          <p>s11: {registerSet.s11}</p>
+        </div>
+        <div>
+          <p>a0: {registerSet.a0}</p>
+          <p>a1: {registerSet.a1}</p>
+          <p>a2: {registerSet.a2}</p>
+          <p>a3: {registerSet.a3}</p>
+          <p>a4: {registerSet.a4}</p>
+          <p>a5: {registerSet.a5}</p>
+          <p>a6: {registerSet.a6}</p>
+          <p>a7: {registerSet.a7}</p>
+        </div>
       </div>
-      <button onClick={executeProgram}>Execution Step</button>
+      {complete ? 
+        <button disabled>Execution Step</button> :
+        <button onClick={executeProgram}>Execution Step</button>
+      }
+      <button onClick={resetEmulator}>Reset</button>
     </div>
   );
 }
